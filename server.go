320cbe806af84af0adf2181a7cae51d5528cc3e3
Paul Nelson <Pablosan@SalientBlue.com>
2014-09-03 17:07:37 -0500

    Now supports all HTTP verbs, added favicon.ico
    - rename routes/config files to scenes
    - scenes files now match VCR cassettes
    - gofmt'ed server.go
    - scenes.json and scenes.yaml both received major overhauls and are now in
    sync with each other.

--- server.go
+++ server.go
@@ -1,59 +1,90 @@
 package main
 
 import (
-		"encoding/json"
-		"flag"
-		"fmt"
-		"github.com/go-martini/martini"
-		"log"
-		"os"
+	"encoding/json"
+	"flag"
+	"github.com/go-martini/martini"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"reflect"
 )
 
+type Status struct {
+	Message string
+	Code    int `json:",float64"`
+}
+
+type Request struct {
+	Uri     string
+	URN     string
+	Query   string
+	Method  string
+	Headers map[string]interface{}
+	Body    string
+}
+
 type Response struct {
-	Headers		[]map[string]string
-	Status		int
-	Body		string
+	Headers map[string]interface{}
+	Status  Status
+	Body    map[string]interface{}
 }
 
-type Route struct {
-	Route		string
-	Verb		string
-	Response	Response
+type Scene struct {
+	Request  Request
+	Response Response
 }
 
-type Config struct {
-	Routes	[]Route
+type Scenes struct {
+	Version      float64
+	Interactions []Scene
+}
+
+func stringify(theMap map[string]interface{}) string {
+	jsonified, _ := json.Marshal(theMap)
+	return string(jsonified)
 }
 
 func main() {
-		// Command line arguments setup
-        var config_file = flag.String("routes", "config.json", "Path to yaml file defining routes.")
-		flag.Parse()
+	// Command line arguments setup
+	var scenes_file = flag.String("scenes", "scenes.json", "Path to json file defining request/response pairs.")
+	flag.Parse()
 
-		// Try to parse the config file
-		file, _ := os.Open(*config_file)
-		decoder := json.NewDecoder(file)
-		config := Config{}
-		err := decoder.Decode(&config)
-		if err != nil {
-			fmt.Printf("%s is not a valid json configuration file.\n", *config_file)
-			log.Fatal(err)
+	// Try to parse the scenes file
+	file, _ := os.Open(*scenes_file)
+	decoder := json.NewDecoder(file)
+	scenes := Scenes{}
+	err := decoder.Decode(&scenes)
+	if err != nil {
+		log.Printf("%s is not a valid json scenes file.\n", *scenes_file)
+		log.Fatal(err)
+	}
+
+	// Crank up Poser
+	m := martini.Classic()
+	m.Any("/**", func(req *http.Request) (int, string) {
+		for _, scene := range scenes.Interactions {
+			if req.Method == scene.Request.Method && req.URL.Path == scene.Request.URN {
+				log.Printf("Matched method %s and URI %s\n", req.Method, req.URL.Path)
+
+				given_query, _ := url.ParseQuery(req.URL.RawQuery)
+				saved_query, _ := url.ParseQuery(scene.Request.Query)
+
+				if reflect.DeepEqual(given_query, saved_query) {
+					log.Printf("Matched query params %s", req.URL.RawQuery)
+					return scene.Response.Status.Code, stringify(scene.Response.Body)
+
+				} else if reflect.DeepEqual(req.Body, scene.Request.Body) {
+					log.Printf("Matched request body %s", req.Body)
+					return scene.Response.Status.Code, stringify(scene.Response.Body)
+				}
+			}
 		}
 
-		fmt.Println(config.Routes)
-		fmt.Println(config.Routes[0].Route)
-		fmt.Println(config.Routes[0].Verb)
-		fmt.Println(config.Routes[0].Response.Headers)
-		fmt.Println(config.Routes[0].Response.Status)
-		fmt.Println(config.Routes[0].Response.Body)
+		// TODO(pablo): I'm not using the response header. Should I be?
 
-		// Crank up Poser
-        m := martini.Classic()
-		for _, route := range config.Routes {
-				// TODO(pablo): Obviously all routes won't be GETs...
-				m.Get(route.Route, func() string {
-						return route.Response.Body
-				})
-		}
-        m.Run()
+		return 501, "ERROR: Your request did not match any scenes."
+	})
+	m.Run()
 }
