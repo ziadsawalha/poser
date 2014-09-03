44c74dc5112abf6c56a2f81e7d1f5e738d3d8ce4
Paul Nelson <Pablosan@SalientBlue.com>
2014-09-02 21:22:19 -0500

    Get routes from passed-in config file

--- server.go
+++ server.go
@@ -1,11 +1,59 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+		"encoding/json"
+		"flag"
+		"fmt"
+		"github.com/go-martini/martini"
+		"log"
+		"os"
+)
+
+type Response struct {
+	Headers		[]map[string]string
+	Status		int
+	Body		string
+}
+
+type Route struct {
+	Route		string
+	Verb		string
+	Response	Response
+}
+
+type Config struct {
+	Routes	[]Route
+}
 
 func main() {
-  m := martini.Classic()
-  m.Get("/", func() string {
-    return "<h2>Hello from Poser!</h2>"
-  })
-  m.Run()
+		// Command line arguments setup
+        var config_file = flag.String("routes", "config.json", "Path to yaml file defining routes.")
+		flag.Parse()
+
+		// Try to parse the config file
+		file, _ := os.Open(*config_file)
+		decoder := json.NewDecoder(file)
+		config := Config{}
+		err := decoder.Decode(&config)
+		if err != nil {
+			fmt.Printf("%s is not a valid json configuration file.\n", *config_file)
+			log.Fatal(err)
+		}
+
+		fmt.Println(config.Routes)
+		fmt.Println(config.Routes[0].Route)
+		fmt.Println(config.Routes[0].Verb)
+		fmt.Println(config.Routes[0].Response.Headers)
+		fmt.Println(config.Routes[0].Response.Status)
+		fmt.Println(config.Routes[0].Response.Body)
+
+		// Crank up Poser
+        m := martini.Classic()
+		for _, route := range config.Routes {
+				// TODO(pablo): Obviously all routes won't be GETs...
+				m.Get(route.Route, func() string {
+						return route.Response.Body
+				})
+		}
+        m.Run()
 }
