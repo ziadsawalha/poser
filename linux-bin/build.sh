2330fe9f393061784226f9656060d13b40e35a52
Paul Nelson <Pablosan@SalientBlue.com>
2014-09-04 12:21:35 -0500

    Rebuild linux binary

--- linux-bin/build.sh
+++ linux-bin/build.sh
@@ -1,5 +1,5 @@
 docker build --rm=true -t pablosan/poser-linux .
 docker run -it -v /var/bin:/gopath/bin --name poser-linux pablosan/poser-linux
-cp /var/bin/poser ../bin/poser
+cp /var/bin/poser ../bin/linux/poser
 docker rm poser-linux
 docker rmi pablosan/poser-linux
