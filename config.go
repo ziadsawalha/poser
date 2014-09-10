781583fa65969f112a5bddb7ccbb8d597e954947
Paul Nelson <pablosan@salientblue.com>
2014-09-10 15:02:52 -0500

    Partially addresses #3: configure/validate proxy

--- config.go
+++ config.go
@@ -11,17 +11,28 @@ var proxy string      // For record mode, the URL Poser should proxy to
 var recordMode bool   // false = Record Mode disabled (default)
 var scenesFile string // The json/yaml file containing all scenes
 
-func parseFlags() {
+func parseConfig() {
 	flag.StringVar(&scenesFile, "scenes", "scenes.json",
 		"Path to json/yaml scenes file. Default is 'scenes.json'")
 	flag.StringVar(&port, "port", "3000", "HTTP port. Defaults to 3000")
 	flag.BoolVar(&playMode, "play", false, "Enable play mode. Default is `true`")
 	flag.BoolVar(&recordMode, "record", false, "Enable record mode. Default is `false`")
-	flag.StringVar(&proxy, "proxy", "http://0.0.0.0",
-		"The URL for which Poser should act as proxy. Default is 'http://0.0.0.0'")
+	flag.StringVar(&proxy, "proxy", "",
+		"The URL for which Poser should act as proxy. Default is \"\"")
 
 	flag.Parse()
 	parseScenes(scenesFile)
+
+	if proxy != "" {
+		// Command line takes precedence: overwrite BaseURL
+		allScenes.BaseURL = proxy
+	}
+
+	if recordMode && allScenes.BaseURL == "" {
+		log.Fatal("A proxy is required for record mode. Provide a valid URL " +
+			"either by adding it to your scenes file under the \"base_url\" " +
+			"key or pass it in using the -proxy command line parameter.")
+	}
 
 	if !(recordMode || playMode) {
 		log.Fatal("Please enable at least one mode and try again (-record and/or -play).")
--- server.go
+++ server.go
@@ -7,21 +7,21 @@ import (
 	"github.com/go-martini/martini"
 )
 
-var version = "v0.0.1"   // Poser version
+var version = "v0.0.1" // Poser version
 
 func main() {
-	parseFlags()
+	parseConfig()
 
 	// Configure Poser
 	m := martini.Classic()
-
 	m.Any("/**", handleAny)
 
 	// Output version, port, and enabled modes information
 	log.Printf("===>>> Poser %s listening on %s", version, port)
 
 	if recordMode {
-		log.Printf("--->>> Poser's record/proxy mode is enabled for %s", allScenes.BaseURL)
+		log.Println("--->>> Poser's record/proxy mode is enabled for:")
+		log.Printf("       %s", allScenes.BaseURL)
 	}
 
 	if playMode {
